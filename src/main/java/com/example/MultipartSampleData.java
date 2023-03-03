b0bdd7d04176fb06ff51fd8156a3a689501f2bc5
alex <alex.pajaron@vier.ai>
2023-03-03 15:01:23 +0100

    first commit

---
+++ src/main/java/com/example/MultipartSampleData.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import lombok.Data;
+
+import java.io.InputStream;
+import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
+
+@Data
+public class MultipartSampleData {
+    @RestForm("file")
+    @PartType("text/csv")
+    InputStream part;
+
+    @RestForm("test")
+    String test;
+
+    @RestForm("checked")
+    boolean checked = false;
+
+    @RestForm("choice")
+    Choice choice = Choice.NO;
+
+}
